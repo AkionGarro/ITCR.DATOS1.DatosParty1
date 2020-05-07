a6f9bbdc2582601993acd742ac232c646387bc72
Carlos Akion Garro Campos <carloscamp1008@estudiantec.cr>
2020-05-07 11:17:46 -0600

    DP1-48
    The game window was created with all the functionalities of checking the same names and empty fields.

--- DatosParty1/src/game/Main.java
+++ DatosParty1/src/game/Main.java
@@ -1,10 +1,15 @@
-
 package game;
 
-
 public class Main {
+
     public static void main(String[] args) {
-        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new Window().setVisible(true);
+            }
+        });
+
     }
-    
+
 }
