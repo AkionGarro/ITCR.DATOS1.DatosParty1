7fcdba99a2398de62d4ea9b9d2f9a40d7cf9f127
Carlos Akion Garro Campos <carloscamp1008@estudiantec.cr>
2020-05-10 14:06:36 -0600

    DP1-58 DP1-59 Random dices function
    The animation of the dice was finished and the function of displaying the dice randomly was added.

--- DatosParty1/src/game/Board.java
+++ DatosParty1/src/game/Board.java
@@ -2,6 +2,9 @@ package game;
 
 public class Board extends javax.swing.JFrame {
 
+    private int dice1;
+    private int dice2;
+    private int total;
     public Board() {
 
         initComponents();
@@ -76,6 +79,156 @@ public class Board extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void rollButtonMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_rollButtonMouseClicked
+        randomDice1();
+        randomDice2();
+        
+    }//GEN-LAST:event_rollButtonMouseClicked
+
+    public void randomDice1() {
+
+        dice1 = (int) (Math.random() * 6 + 1);
+        if (dice1 == 1) {
+            leftDice1.setVisible(true);
+            leftDice2.setVisible(false);
+            leftDice3.setVisible(false);
+            leftDice4.setVisible(false);
+            leftDice5.setVisible(false);
+            leftDice6.setVisible(false);
+            return;
+        }
+        if (dice1 == 2) {
+            leftDice1.setVisible(false);
+            leftDice2.setVisible(true);
+            leftDice3.setVisible(false);
+            leftDice4.setVisible(false);
+            leftDice5.setVisible(false);
+            leftDice6.setVisible(false);
+
+            return;
+        }
+        if (dice1 == 3) {
+            leftDice1.setVisible(false);
+            leftDice2.setVisible(false);
+            leftDice3.setVisible(true);
+            leftDice4.setVisible(false);
+            leftDice5.setVisible(false);
+            leftDice6.setVisible(false);
+
+            return;
+        }
+        if (dice1 == 4) {
+            leftDice1.setVisible(false);
+            leftDice2.setVisible(false);
+            leftDice3.setVisible(false);
+            leftDice4.setVisible(true);
+            leftDice5.setVisible(false);
+            leftDice6.setVisible(false);
+
+            return;
+        }
+        if (dice1 == 5) {
+            leftDice1.setVisible(false);
+            leftDice2.setVisible(false);
+            leftDice3.setVisible(false);
+            leftDice4.setVisible(false);
+            leftDice5.setVisible(true);
+            leftDice6.setVisible(false);
+
+            return;
+        }
+        if (dice1 == 6) {
+            leftDice1.setVisible(false);
+            leftDice2.setVisible(false);
+            leftDice3.setVisible(false);
+            leftDice4.setVisible(false);
+            leftDice5.setVisible(false);
+            leftDice6.setVisible(true);
+
+            return;
+        }
+
+    }
+
+    public void randomDice2() {
+
+        dice2 = (int) (Math.random() * 6 + 1);
+
+        if (dice2 == 1) {
+            rightDice1.setVisible(true);
+            rightDice2.setVisible(false);
+            rightDice3.setVisible(false);
+            rightDice4.setVisible(false);
+            rightDice5.setVisible(false);
+            rightDice6.setVisible(false);
+
+            return;
+        }
+        if (dice2 == 2) {
+            rightDice1.setVisible(false);
+            rightDice2.setVisible(true);
+            rightDice3.setVisible(false);
+            rightDice4.setVisible(false);
+            rightDice5.setVisible(false);
+            rightDice6.setVisible(false);
+
+            return;
+        }
+        if (dice2 == 3) {
+            rightDice1.setVisible(false);
+            rightDice2.setVisible(false);
+            rightDice3.setVisible(true);
+            rightDice4.setVisible(false);
+            rightDice5.setVisible(false);
+            rightDice6.setVisible(false);
+
+            return;
+        }
+        if (dice2 == 4) {
+            rightDice1.setVisible(false);
+            rightDice2.setVisible(false);
+            rightDice3.setVisible(false);
+            rightDice4.setVisible(true);
+            rightDice5.setVisible(false);
+            rightDice6.setVisible(false);
+
+            return;
+        }
+        if (dice2 == 5) {
+            rightDice1.setVisible(false);
+            rightDice2.setVisible(false);
+            rightDice3.setVisible(false);
+            rightDice4.setVisible(false);
+            rightDice5.setVisible(true);
+            rightDice6.setVisible(false);
+
+            return;
+        }
+        if (dice2 == 6) {
+            rightDice1.setVisible(false);
+            rightDice2.setVisible(false);
+            rightDice3.setVisible(false);
+            rightDice4.setVisible(false);
+            rightDice5.setVisible(false);
+            rightDice6.setVisible(true);
+
+            return;
+        }
+    }
+
+    public int getDice1() {
+        return dice1;
+    }
+
+    public int getDice2() {
+        return dice2;
+    }
+
+    public int getPlayerMove() {
+        total = getDice1()+getDice2();
+        return total;
+    }
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
