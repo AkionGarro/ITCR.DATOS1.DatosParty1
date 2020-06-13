d665ea864fc9af2b3e2f2eb08491fb63083b4cdb
NachoGranados <ignaciogramar@gmail.com>
2020-06-13 01:23:07 -0600

    Fixing little bug in the endGame method.

--- DatosParty1/src/games/MathQuiz/game/MathQuiz.java
+++ DatosParty1/src/games/MathQuiz/game/MathQuiz.java
@@ -997,6 +997,8 @@ public class MathQuiz extends javax.swing.JFrame {
      */
     private void exitButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_exitButtonActionPerformed
         
+        /*
+        
         switch (option) {
             
             case 2:
@@ -1057,6 +1059,17 @@ public class MathQuiz extends javax.swing.JFrame {
                 
                 break;
                 
+        }
+        
+        */
+        
+        dispose();
+        
+        if (Board.round == 13) {
+            
+            Board.mingameFinished = true;
+            Board.checkEndGame();
+            
         }
         
     }//GEN-LAST:event_exitButtonActionPerformed
